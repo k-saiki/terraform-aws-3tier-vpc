655184b94b01e404206f0ff98beff56bbcfa1505
saiki.ko <saiki.ko@classmethod.jp>
2018-03-18 01:46:22 +0900

    Modify example

--- example/vpc.tf
+++ example/vpc.tf
@@ -12,7 +12,7 @@ module "vpc" {
   protected_subnets = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
   private_subnets   = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway                = false
+  enable_nat_gateway                = true
   single_nat_gateway                = false
   enable_dhcp_options               = false
   dhcp_options_domain_name          = "example.local"
